4c404cfbffc5b2d0e83362dd486420e7c31fe291
rahulroshan96 <rahulroshan96@gmail.com>
2015-10-11 05:26:34 +0530

    update

--- geekforgeeks/string/string project/14 searching for pattern.cpp
+++ geekforgeeks/string/string project/14 searching for pattern.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 void find_pattern(string &s1, string &s2)
 {
+    vector<int> table(s1.length(),0);
+    for(int x=0;x<s1.length();++x)
+    {
+
     int i=0,j=0,m=0,index=0;
     bool flag = false;
-    for(i=0;i<s1.length();++i)
+    for(i=x;i<s1.length();++i)
     {
         if(s1[i] == s2[m] && flag==false)
         {
@@ -16,9 +20,14 @@ void find_pattern(string &s1, string &s2)
         }
         else if(s1[i]==s2[m] && flag==true)
         {
-            if(s1[i] == s2[s2.length()-1]) // means this is the last character of s2 string
+            //if(s1[i] == s2[s2.length()-1]) // means this is the last character of s2 string
+            if((i-index+1) == s2.length()) // means this is the last character of s2 string
             {
-                cout<<"pattern found at "<<index;
+                if(table[index] == 0)
+                {
+                    cout<<"pattern found at "<<index<<endl;
+                    table[index] = 1;
+                }
 
                 flag=false;
                 m=0;
@@ -35,12 +44,13 @@ void find_pattern(string &s1, string &s2)
         }
 
     }
+    }
 }
 
 int main()
 {
-    string s1("rahul isx the best isx");
-    string s2("is");
+    string s1("aaaaaaaa");
+    string s2("aaaa");
 
     find_pattern(s1,s2);
 
