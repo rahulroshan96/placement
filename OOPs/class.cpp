3a4dc20414aa716585024867976d2c92033e9930
rahul <rahulroshan96@gmail.com>
2016-06-30 15:22:09 +0530

    update

--- OOPs/class.cpp
+++ OOPs/class.cpp
@@ -27,11 +27,19 @@ public:
 	{
 		food = s;
 	}
+	// These are inline functions, which in case of big code leads to Code Bloat
 	string getFood()
 	{
 		return food;
 	}
+	// Declare here is must
+	void show();
 };
+
+// Another way to define one function, outside the class, decalration in class is must
+void Animal::show(){
+	cout<<"Test";
+}
 
 int main()
 {
@@ -43,6 +51,8 @@ int main()
 	cout<<"chini has "<<chini.getLegs()<<" legs\n";
 	cout<<"chini speaks "<<chini.getSound()<<endl;
 	cout<<"chini eats "<<chini.getFood()<<endl;
+	
+	chini.show();
 
 	return 0;
 
