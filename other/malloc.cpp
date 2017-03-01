51e21cc0eb58d3170f1f80073eb0ef1d563f885d
Umesh Mali <Umesh Mali>
2017-03-02 02:06:38 +0530

    malloc documentation added

---
+++ other/malloc.cpp
@@ -0,0 +1,13 @@
+// https://www.cs.princeton.edu/courses/archive/fall06/cos217/lectures/14Memory-2x2.pdf
+//http://www.inf.udec.cl/~leo/Malloc_tutorial.pdf
+// http://stackoverflow.com/questions/32388847/understanding-and-implementing-malloc
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+int main(){
+	void *p;
+	size_t s = 100;
+	p = sbrk(s);
+	return 0;
+}
