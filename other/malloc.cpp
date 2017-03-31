ca40574d8bb6917f5723ddba9a43ede696d5ad49
Rahul Roshan <rahulroshan96@gmail.com>
2017-03-31 15:27:01 +0530

    Update malloc.cpp

--- other/malloc.cpp
+++ other/malloc.cpp
@@ -1,6 +1,7 @@
 // https://www.cs.princeton.edu/courses/archive/fall06/cos217/lectures/14Memory-2x2.pdf
 //http://www.inf.udec.cl/~leo/Malloc_tutorial.pdf
 // http://stackoverflow.com/questions/32388847/understanding-and-implementing-malloc
+// https://danluu.com/malloc-tutorial/
 #include <iostream>
 #include <unistd.h>
 
